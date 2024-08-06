0a394ee650f2d14a3695ff8ccb3fee29c6766b56
Ygor da Rosa da Rosa <yygorrosa@gmail.com>
2024-08-06 11:16:02 -0300

    feat: adicionado o front

---
+++ src/js/EventController.js
@@ -0,0 +1,21 @@
+
+
+class EventController{
+  constructor(){
+    this.allLogsForm = document.getElementById("all-logs-form")
+    this.monthLogsForm = document.getElementById("month-logs-form")
+    this.initButtons()
+  }
+
+
+  initSubmitForm(){
+    this.allLogsForm.addEventListener("submit", (event) => {
+
+    })
+  }
+}
+
+
+export default EventController
+
+
